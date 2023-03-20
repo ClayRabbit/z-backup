bd602d6d3c3b62b6b7993a54e6ad5239b285ba25
IQ <iq@backup.iq.spb.ru>
2023-03-20 12:39:23 +0000

    sudo added

--- prepare.sh
+++ prepare.sh
@@ -7,9 +7,9 @@ ssh-keygen -t rsa -N "" -f ~/.ssh/backup.key
 pubkey=$(cat ~/.ssh/backup.key)
 
 echo <<EOF
-# execute following command on backup server (assuming "backup" is zfs pool for backups):
-zfs create "backup/$1"
-adduser "$1" --disabled-password
-zfs allow "$1" snapshot,destroy "backup/$1"
-su "$1" -c "echo '$pubkey' >> '/home/$1/.ssh/authorized_keys
+# execute following commands on backup server (assuming "backup" is zfs pool for backups):
+sudo zfs create "backup/$1"
+sudo adduser "$1" --disabled-password
+sudo zfs allow "$1" snapshot,destroy "backup/$1"
+sudo su "$1" -c "echo '$pubkey' >> '/home/$1/.ssh/authorized_keys
 EOF
