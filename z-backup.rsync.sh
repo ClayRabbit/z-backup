4757ef50df34cb455fe88b4fb5d20b1d1cc143a3
Andrey Chesnakov <garlic@mail.ru>
2024-01-12 22:59:46 +0500

    Update z-backup.rsync.sh

--- z-backup.rsync.sh
+++ z-backup.rsync.sh
@@ -3,6 +3,9 @@ SSH="ssh"
 if [ -n "$3" ]; then
     SSH="$3"
 fi
+[ ! -e "./rsync.exclude" ] && touch ./rsync.exclude
+[ ! -e "./rsync.include" ] && touch ./rsync.include
+
 rsync -e "$SSH" -avz --numeric-ids --no-specials --no-devices --hard-links --delete --delete-after --delete-excluded --inplace -M--fake-super \
     --exclude=/dev --exclude=/proc --exclude=/sys --exclude=/selinux --exclude=/cgroups --exclude=lost+found \
     --exclude='/tmp/*' --exclude='/var/tmp/*' --exclude='/run/*' --exclude='/var/run/*' --exclude='/var/log/*' --exclude=/var/lib/lxcfs/ \
